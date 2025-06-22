805be81e4e7ccc04739db368655ac9b8e7699fed
s4ndm4n <sandaruwan12@gmail.com>
2025-06-22 14:02:14 +0530

    Started coding the app.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:learn_local_s24013191_sandaruwan_samaraweera/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const LearnLocalApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
